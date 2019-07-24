f83e2e3517115e20751b940c07a726d95f2c3a05
快乐的我531 <2302004040@qq.com>
2019-07-24 15:56:18 +0800

    适配更多分辨率

--- 图片转影壁.js
+++ 图片转影壁.js
@@ -1,19 +1,57 @@
 //使用auto.js 4.0.1 beta版本 编写&运行
-//////如果你的手机分辨率不是3120x1440，请修改这些值
 
-const pixelWidth = 16; //用比最小笔刷宽度大一点点的宽度点一个点，这个点的直径
-const printAreaBegin = new Array(1350, 343); //绘图区左上角坐标
-const printAreaEnd = new Array(2768, 1138); //绘图区右下角坐标
-const colorSelecterX = 1150; //选择颜色区的x坐标(正中间)
-const colorSelecterY = new Array(430, 595, 765, 930, 1090, 720, 880, 1050); //选择颜色区各个颜色对应的y坐标，最后3个需要向下翻页到底再获取
+//输入图片的路径(需要提前缩放到..176x94(在我的手机上))
+const img = images.read("/sdcard/test.png"); 
 
-const img = images.read("/sdcard/test3.png"); //输入的图片(需要提前缩放到..176x94(在我的手机上))
+//请在开始运行之前，切换到画板的"画刷"页面，并且调整滑块到最细的一端稍往上一点的位置！
 
-//////结束
+//////一些预置的分辨率
+
+if (device.height == 3120 && device.width == 1440) {
+    //3120x1440(eg.LG G7)
+    var pixelWidth = 16;
+    var printAreaBegin = [1350, 343];
+    var printAreaEnd = [2768, 1138];
+    var colorSelecterX = 1150;
+    var colorSelecterY = [430, 595, 765, 930, 1090, 720, 880, 1050];
+
+} else if (device.height == 1920 && device.width == 1080) {
+    //1920x1080(eg.小米5s)   
+    var pixelWidth = 12;
+    var printAreaBegin = [769, 258];
+    var printAreaEnd = [1831, 853];
+    var colorSelecterX = 620;
+    var colorSelecterY = [320, 450, 570, 690, 806, 534, 660, 787];
+
+} else if (device.height == 2160 && device.width == 1080) {
+    //2160x1080(来自酷安网友)
+    var pixelWidth = 12;
+    var printAreaBegin = [890, 258];
+    var printAreaEnd = [1951, 853];
+    var colorSelecterX = 735;
+    var colorSelecterY = [320, 450, 570, 690, 806, 534, 660, 787];
+} else if (device.height == 2340 && device.width == 1080) {
+    //2340x1080(eg.红米k20pro)
+    var pixelWidth = 12;
+    var printAreaBegin = [980, 258];
+    var printAreaEnd = [2040, 853];
+    var colorSelecterX = 825;
+    var colorSelecterY = [320, 450, 570, 690, 806, 534, 660, 787];
+} else {
+    //暂时没适配的分辨率，你可以自己更改这个脚本
+    toast("暂不支持此分辨率！");
+    toast("你也可以打开脚本自行适配");
+    exit();
+    var pixelWidth = 16; //用比最小笔刷宽度大一点点的宽度点一个点，这个点的直径
+    var printAreaBegin = new Array(1350, 343); //绘图区左上角坐标
+    var printAreaEnd= new Array(2768, 1138); //绘图区右下角坐标
+    var colorSelecterX = 1150; //选择颜色区的x坐标(正中间)
+    var colorSelecterY = new Array(430, 595, 765, 930, 1090, 720, 880, 1050); //选择颜色区各个颜色对应的y坐标，最后3个需要向下翻页到底再获取
+};
 
 const pixelGap = pixelWidth / 2;
-const maxWidth = (printAreaEnd[0] - printAreaBegin[0] - pixelWidth) / pixelWidth;
-const maxHeight = (printAreaEnd[1] - printAreaBegin[1] - pixelWidth) / pixelWidth;
+const maxWidth = (printAreaEnd[0] - printAreaBegin[0] - pixelWidth) / pixelGap;
+const maxHeight = (printAreaEnd[1] - printAreaBegin[1] - pixelWidth) / pixelGap;
 
 const colorTable = new Array("#FFFDFFFF", "#FFE7B81A", "#FF1BE6E4", "#FFE71A62", "#FFB51AE6", "#FF1BE675", "#FF010000", "#FF3700A7"); //画板里仅有的几个颜色(差评)
 
@@ -46,12 +84,20 @@ function switchColor(colId, needSwipe) { //更换当前笔刷颜色
 };
 
 if (img == null) {
-    toast("input file is NULL!");
+    toast("输入图片错误！请检查图片路径与格式");
+    exit();
 };
 
 var pixelCountX = img.getWidth();
 var pixelCountY = img.getHeight();
-
+if (pixelCountX>maxWidth){
+    toast("图片宽度过大！最大为"+maxWidth);
+    //exit();
+    };
+    if (pixelCountY>maxHeight){
+    toast("图片高度过大！最大为"+maxHeight);
+    //exit();
+    };
 
 var prevColId = 0;
 for (var i = 1; i <= pixelCountX; i++) {
