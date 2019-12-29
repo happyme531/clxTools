d056ec2babdff6efeced583804469f65b516956b
happyme531 <2302004040@qq.com>
2019-12-29 20:57:07 +0800

    ...

--- 楚留香自动日常/main.js
+++ 楚留香自动日常/main.js
@@ -3,14 +3,23 @@ var tasks=new Array();
 tasks.push(require('./src/modules/钱庄.js'));
 tasks.push(require('./src/modules/每日buff.js'));
 tasks.push(require('./src/modules/豪杰.js'));
+tasks.push(require('./src/modules/门派任务.js'));
+tasks.push(require('./src/modules/论剑.js'));
 console.show();
+console.setSize(600, 400);
+console.setPosition(600,400 )
+sleep((5000));
 
+
+
+tasks[4].steps[0].run()
+exit();
 let i=2;
 for (let j = 0; j < tasks[i].steps.length; j++) {
     !tasks[i].steps[j].run()?exit():0;
     sleep(1000);
 }
-exit();
+
 
 for (let i = 0; i < tasks.length; i++) {
     for (let j = 0; j < tasks[i].steps.length; j++) {
--- 楚留香自动日常/src/utils/screenutil.js
+++ 楚留香自动日常/src/utils/screenutil.js
@@ -5,8 +5,23 @@ screenutil.checkColor = function (x,y,col) {
         console.verbose("颜色检测成功:图片%s,目标%s",colors.toString(images.pixel(img,x,y)),col)
         return 1;
     }else {
+        console.verbose("颜色检测失败:图片%s,目标%s",colors.toString(images.pixel(img,x,y)),col)
         return 0;
     };
         
+};
+screenutil.waitUntilColorMatch =function(x, y, col,minTry){
+    minTry=minTry||5;
+    let findingEnd = 0;
+    while (findingEnd<minTry) {
+        sleep(800);
+        if (screenutil.checkColor(x,y, col)) {
+            findingEnd++;
+            console.verbose("颜色对应成功:第%d次", findingEnd);
+        } else {
+            findingEnd = 0;     
+        }
+    };
+
 };
 module.exports=screenutil;
